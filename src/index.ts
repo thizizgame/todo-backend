4933a5707b0f7bfa8bd0285c90ac07342760935b
25LP4949 25LP4949 <25LP4949@G301-CC0194.local>
2025-10-01 12:15:01 +0800

    mode

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import { nanoid } from "nanoid";
 const app: Application = express();
 const port = 3000;
 
-const tasks = [
-    { id: nanoid(), name: "Shalaa ugaah" },
-    { id: nanoid(), name: "Toosoo archih" },
+let tasks = [
+    { id: nanoid(), name: "Shalaa ugaah", mode: false },
+    { id: nanoid(), name: "Toosoo archih", mode: false },
 ];
 app.use(cors());
 app.use(express.json());
@@ -21,12 +21,30 @@ app.get('/tasks', (req: Request, res: Response) => {
 app.post('/tasks', (req: Request, res: Response) => {
     const id = nanoid()
     const { name } = req.body;
-    tasks.unshift({ id, name });
+    const { mode } = req.body;
+    tasks.unshift({ id, name, mode });
     res.status(201).send({ id });
 })
 app.delete('/tasks/:id', (req: Request, res: Response) => {
     const id = req.params.id
 
+    const newTasks = tasks.filter((task) => task.id !== id)
+    tasks = newTasks;
+    res.send(tasks)
+})
+app.put('/tasks/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    const { name } = req.body;
+    const index = tasks.findIndex(task => task.id === id);
+    tasks[index].name = name;
+    res.send(tasks)
+})
+app.put('/toggle/tasks/:id', (req: Request, res: Response) => {
+    const id = req.params.id
+    const { mode } = req.body
+    const index = tasks.findIndex(task => task.id === id);
+    tasks[index].mode = mode;
+    res.send(tasks)
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
